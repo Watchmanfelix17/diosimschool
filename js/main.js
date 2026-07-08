11120bbbcfbde241a3a8c9ee03082ef26b09f1ca
watchmanfelix17 <watchmanfelix17@gmail.com>
2026-07-08 05:49:49 +0100

    Added About section and fixed Hero layout and copy

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DIOSIMSchool Foundation Loaded Successfully.");
+    
+    // Future logic for the Mobile Navigation Menu will go here
+    
+    // Future logic for the WhatsApp redirect will go here
+});
